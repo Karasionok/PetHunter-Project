ed1503809d719d73c49cb5b1d4c1d80d51e67c7c
Karasionok <daniilokladnikov@mail.ru>
2024-09-28 21:21:11 +0300

    In the beginning...

--- main.py
+++ main.py
@@ -3,25 +3,12 @@ from flask_login import LoginManager, login_user, login_required, logout_user, c
 from flask import Flask
 
 app = Flask(__name__)
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return 'Hello'
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    pass
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    pass
+def home():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=5000, host='127.0.0.1')
