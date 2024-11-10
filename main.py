9625154dfcf4015dd017650ff6f5fbe6c8903e88
karasionok <daniilokladnikov@mail.ru>
2024-11-11 00:05:26 +0300

    начал добавлять карту

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, redirect, request, session, Blueprint
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from flask import Flask
+from flask import Flask, render_template
+import folium
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
+@app.route("/index")
 def home():
-    return render_template('home.html')
+    mapObj = folium.Map(location=[55.800595, 37.473519], zoom_start=14)
+    folium.GeoJson("shukino.geojson").add_to(mapObj)
+    mapObj.get_root().render()
+    header = mapObj.get_root().header.render()
+    body = mapObj.get_root().html.render()
+    script = mapObj.get_root().script.render()
+
+    return render_template("index.html", header=header, body=body, script=script)
 
 
 if __name__ == '__main__':
-    app.run(port=5000, host='127.0.0.1')
+    app.run(port=8080, host='127.0.0.1')
