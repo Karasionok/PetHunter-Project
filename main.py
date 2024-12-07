76ac4e24b3490e23c201546daa0ec82d162f9ca9
Karasionok <daniilokladnikov@mail.ru>
2024-12-07 11:29:20 +0300

    TO FIX: not register IN PROCESS: add page

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from flask import Flask, render_template, request, redirect
 
 import folium       # Libraries for map
 
-from sqlalchemy import create_engine    # Libraries for DataBase
+from sqlalchemy import create_engine, select    # Libraries for DataBase
 from sqlalchemy.orm import Session
 
 from models.sqlmodels import User         # Imports from other files
 
 
 app = Flask(__name__)
-engine = create_engine("sqlite:///DB/PetHunt.db")
+engine = create_engine("sqlite:///DB/PetHunt.db", echo=True)    # page initial
 
 
-@app.route("/")
+@app.route("/")         # Main Page
 @app.route("/index")
 def home():
     mapObj = folium.Map(location=[55.800595, 37.473519], zoom_start=14)
@@ -27,7 +27,7 @@ def home():
     return render_template("index.html", header=header, body=body, script=script)
 
 
-@app.route("/register", methods=["POST", "GET"])
+@app.route("/register", methods=["POST", "GET"])        # register page
 def register():
     if flask.request.method == "POST":
         log = request.form['lg']
@@ -35,7 +35,7 @@ def register():
         fio = request.form['fio']
         phone = request.form['phone']
         ds = request.form['ds']
-        if log != '' and pas != '':
+        if log is not None and pas is not None:
             print(log, pas, fio, phone, ds)
             with Session(engine) as session:
                 usr = User(
@@ -52,15 +52,24 @@ def register():
         return render_template("register.html")
 
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route("/login", methods=["POST", "GET"])           # login page
 def login():
     if flask.request.method == "POST":
         log = request.form['log']
         pas = request.form['pas']
         print(log, pas)
+        with Session(engine) as session:
+            stmt = select(User).where(User.login.in_(["log"]))
+            for user in session.scalars(stmt):
+                print(user)
         return redirect('/index')
     else:
         return render_template("login.html")
+
+
+@app.route("/add", methods=["POST", "GET"])         # add page
+def add():
+    return render_template("add.html")
 
 
 if __name__ == '__main__':
